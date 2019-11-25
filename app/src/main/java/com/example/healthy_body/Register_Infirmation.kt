6f7ab1f52fbb78d9c124dc09d9e2da4c3f71cfdf
MarieData <marie.fb1245@gmail.com>
2019-11-26 00:43:26 +0700

    refactor(Mobile):create settingUI

--- app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
+++ app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
@@ -3,9 +3,12 @@ package com.example.healthy_body
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner
+import kotlinx.android.synthetic.main.activity_register__infirmation.*
+import kotlinx.android.synthetic.main.activity_totalkcal__user.*
 import java.util.*
 
 class Register_Infirmation : AppCompatActivity() {
@@ -15,13 +18,31 @@ class Register_Infirmation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register__infirmation)
-
         val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,SPINNERLST)
         val betterSpinner = findViewById(R.id.android_material_desgn_spinner) as MaterialBetterSpinner
         betterSpinner.setAdapter(arrayAdapter)
+        registerbutton.setOnClickListener {
+            val text = betterSpinner.text.toString()
+            Log.d("test","pass")
+            test(text)
+            val intent = Intent(this, Totalkcal_User::class.java)
+            startActivity(intent)
+
+        }
     }
-    fun register (v: View){
-        val intent = Intent(this, Totalkcal_User::class.java)
-        startActivity(intent)
+
+
+  //  fun register (v: View){
+
+    //    val text = betterSpinner.text.toString()
+    //    Log.d("test","pass")
+
+    // /   test(text)
+  //      val intent = Intent(this, Totalkcal_User::class.java)
+   //     startActivity(intent)
+   // }
+
+    fun test (text:String){
+        Log.e("test", "index="+text)
     }
 }
---
+++ app/src/main/java/com/example/healthy_body/setting_user.kt
@@ -0,0 +1,12 @@
+package com.example.healthy_body
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class setting_user : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_user)
+    }
+}
