a38c7fd8445f9e8437dee67351a2aec529305d2d
MarieData <marie.fb1245@gmail.com>
2019-11-25 13:01:56 +0700

    refactor(Moble):set data dropdown page register_information and create activity Login_user

--- app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
+++ app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
@@ -4,14 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner
+import java.util.*
 
 class Register_Infirmation : AppCompatActivity() {
 
+    internal var SPINNERLST = arrayOf("low workout","normal workout to 1-3 time a week","normal workout to 4-5 time a week",
+        "heavy workout to 6-7 time a week","heaviest workout over to 2 time a day")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register__infirmation)
 
-
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,SPINNERLST)
+        val betterSpinner = findViewById(R.id.android_material_desgn_spinner) as MaterialBetterSpinner
+        betterSpinner.setAdapter(arrayAdapter)
     }
     fun register (v: View){
         val intent = Intent(this, Totalkcal_User::class.java)
--- app/src/main/java/com/example/healthy_body/connect_user.kt
+++ app/src/main/java/com/example/healthy_body/connect_user.kt
@@ -11,8 +11,14 @@ class connect_user : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connect_user)
         val b2 = findViewById(R.id.b2) as Button
+        val b1 = findViewById(R.id.b1) as Button
         b2.setOnClickListener {
             val intent = Intent(this, Register_User::class.java)
+            startActivity(intent)
+
+        }
+        b1.setOnClickListener {
+            val intent = Intent(this, Login_user::class.java)
             startActivity(intent)
 
         }
