4b3789920be586b5e049b446128872a79b5fa8e2
MarieData <marie.fb1245@gmail.com>
2019-12-02 23:20:28 +0700

    refactor(Mobile):เเก้สมการ TDEE

--- app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
+++ app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
@@ -14,11 +14,15 @@ import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.annotation.IntegerRes
+import java.math.RoundingMode
 
 import kotlin.math.pow
 
 import java.text.DecimalFormat
-
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import kotlin.math.ceil
 
 
 class Register_Infirmation : AppCompatActivity() {
@@ -36,14 +40,14 @@ class Register_Infirmation : AppCompatActivity() {
         val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,SPINNERLST)
         val betterSpinner = findViewById(R.id.android_material_desgn_spinner) as MaterialBetterSpinner
         betterSpinner.setAdapter(arrayAdapter)
-var text = "low workout"
+
 
         registerbutton.setOnClickListener {
-            var resultSPIN = betterSpinner.text.toString()
+
             var value = radiogroup.checkedRadioButtonId
             var radioG = findViewById<RadioButton>(value)
             val df = DecimalFormat("0.00")
-
+            val resultSPIN = betterSpinner.text.toString()
             var textheight = findViewById<EditText>(R.id.inputheight)
             var textweigth = findViewById<EditText>(R.id.inputweight)
             var textage = findViewById<EditText>(R.id.inputage)
@@ -55,8 +59,36 @@ var text = "low workout"
             val BMIS = df.format(totalBMI)
             val BMI = java.lang.Float.valueOf(BMIS)
             var gender  = radioG.text.toString()
+
+
+
+            Log.d("information","resultSPIN:$resultSPIN")
             Log.d("information","age:$age")
             Log.d("information","gender :$gender")
+
+             if(gender == "ชาย"){
+                 Log.e("information","pass")
+                 val result = 66+(13.7f*weigth)+(5*height)-(6.8f*age)
+                 val BMR = Math.round(result)
+                 //var textBMR = df.format(result)
+                 val resultTDEE = (BMR * 1.55 )
+                 val TDEE = Math.round(resultTDEE)
+                 val reTDEE = resultTDEE - 500
+                 val TDEEer = Math.round(reTDEE)
+                 Log.d("information","BMR:$BMR")
+                 Log.d("information","reTDEE:$TDEEer")
+                 Log.d("information","TDEE:$TDEE")
+
+
+               // genderboy(weigth,height,age)
+
+
+
+            }else{
+                val BMR = 665f +(9.6f * weigth)+(1.8f * height)-(4.7*age)
+                Log.d("information","BMRหญิง:$BMR Kcal")
+            }
+
 
             //หาBMI
             if(BMI < 23){
@@ -83,23 +115,6 @@ var text = "low workout"
             }
 
             //หาBMR
-            if(gender == "ชาย"){
-                val df = DecimalFormat("0,000")
-                val result = 66+(13.7f*weigth)+(5*height)-(6.8f*age)
-                val textBMR = df.format(result)
-                val BMR =  java.lang.Float.valueOf(textBMR)
-                Log.d("information","BMRชาย:$BMR Kcal")
-
-                //หาTDEE
-               /* if(resultSPIN == text){
-                    var TDEE = 1.2 * BMR
-                    Log.d("information","TDEEชาย:$TDEE Kcal")
-                }*/
-
-            }else{
-                val BMR = 665f +(9.6f * weigth)+(1.8f * height)-(4.7*age)
-                Log.d("information","BMRหญิง:$BMR Kcal")
-            }
 
 
 
@@ -110,8 +125,30 @@ var text = "low workout"
         }
 
 
-    }
 
+}
+
+   /* fun genderboy(weigth:Float,height:Float,age:Float){
+        val result = 66+(13.7f*weigth)+(5*height)-(6.8f*age)
+        val df = DecimalFormat("0,000")
+        df.roundingMode = RoundingMode.CEILING
+        val textBMR = df.format(result)
+        val number = java.lang.Float.valueOf(textBMR)
+        Log.d("information","BMRชาย:$textBMR Kcal")
+        Log.d("information","number:$number Kcal")
+    }*/
+
+    /*fun testTDEE(result:Float,resultSPIN:String){
+        val df = DecimalFormat("0,000")
+     val textBMR = df.format(result)
+        val BMR = java.lang.Float.valueOf(textBMR)
+        if(resultSPIN == "normal workout to 4-5 time a week"){
+            val df = DecimalFormat("0,000")
+            val resultTDEE = 500-(BMR * 1.55)
+            val TDEE = df.format(resultTDEE)
+            Log.d("information","TDEE:$TDEE Kcal")
+        }
+    }*/
 
 
 }
