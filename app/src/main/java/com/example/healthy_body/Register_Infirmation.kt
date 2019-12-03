3827bfa916d4a38590e1e097143607d28327f279
MarieData <marie.fb1245@gmail.com>
2019-12-03 15:16:28 +0700

    refactor(Mobile):เเก้สมการ BMI BMR TDEE เสร็จเเล้ว

--- app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
+++ app/src/main/java/com/example/healthy_body/Register_Infirmation.kt
@@ -22,14 +22,20 @@ import java.text.DecimalFormat
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.icu.text.CaseMap
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_totalkcal__user.*
+import java.util.logging.Level
 import kotlin.math.ceil
+import kotlin.math.log
 
 
 class Register_Infirmation : AppCompatActivity() {
 
     internal var SPINNERLST = arrayOf("low workout","normal workout to 1-3 time a week","normal workout to 4-5 time a week",
         "heavy workout to 6-7 time a week","heaviest workout over to 2 time a day")
-
+    private var myRef = FirebaseAuth.getInstance()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,11 +49,14 @@ class Register_Infirmation : AppCompatActivity() {
 
 
         registerbutton.setOnClickListener {
-
+            var firstname  =  findViewById<EditText>(R.id.inputfirstname)
+            var lastname  =  findViewById<EditText>(R.id.inputLastname)
+            var textfirstname = firstname.text.toString()
+            var textlastname = lastname.text.toString()
             var value = radiogroup.checkedRadioButtonId
             var radioG = findViewById<RadioButton>(value)
             val df = DecimalFormat("0.00")
-            val resultSPIN = betterSpinner.text.toString()
+            val Level_Workout = betterSpinner.text.toString()
             var textheight = findViewById<EditText>(R.id.inputheight)
             var textweigth = findViewById<EditText>(R.id.inputweight)
             var textage = findViewById<EditText>(R.id.inputage)
@@ -61,94 +70,112 @@ class Register_Infirmation : AppCompatActivity() {
             var gender  = radioG.text.toString()
 
 
+            Log.d("information","firstname = $textfirstname")
+            Log.d("information","lastname = $textlastname")
+            //หาBMI
+            if(BMI < 23){
+                 val status = "ปหติ"
+            }else if( BMI >= 23 && BMI <=25){
+                val status = "เริ่มอ่วน"
+            }else if( BMI >= 25 && BMI <=30) {
+                val  status = "อ่วน"
+            }else{
+                val  status = "อ่วนมาก"
+            }
 
-            Log.d("information","resultSPIN:$resultSPIN")
-            Log.d("information","age:$age")
-            Log.d("information","gender :$gender")
+
 
              if(gender == "ชาย"){
                  Log.e("information","pass")
                  val result = 66+(13.7f*weigth)+(5*height)-(6.8f*age)
                  val BMR = Math.round(result)
-                 //var textBMR = df.format(result)
-                 val resultTDEE = (BMR * 1.55 )
-                 val TDEE = Math.round(resultTDEE)
-                 val reTDEE = resultTDEE - 500
-                 val TDEEer = Math.round(reTDEE)
-                 Log.d("information","BMR:$BMR")
-                 Log.d("information","reTDEE:$TDEEer")
-                 Log.d("information","TDEE:$TDEE")
+                 Log.d("information","BMRชาย :$BMR")
+                 //หาค่า TDEE
+                 if(Level_Workout=="low workout"){
+                     val resultTDEE = (BMR * 1.2 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
 
+                 }else if(Level_Workout=="normal workout to 1-3 time a week"){
+                     val resultTDEE = (BMR * 1.375 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
 
-               // genderboy(weigth,height,age)
+                 }else if (Level_Workout=="normal workout to 4-5 time a week"){
+                     val resultTDEE = (BMR * 1.55 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
+
+                 }else if (Level_Workout=="heavy workout to 6-7 time a week"){
+                     val resultTDEE = (BMR * 1.7 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
+                 }else{
+                     val resultTDEE = (BMR * 1.9 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
+                 }
 
 
 
             }else{
-                val BMR = 665f +(9.6f * weigth)+(1.8f * height)-(4.7*age)
+                val result = 665f +(9.6f * weigth)+(1.8f * height)-(4.7f*age)
+                 var BMR = Math.round(result)
+
                 Log.d("information","BMRหญิง:$BMR Kcal")
-            }
+
+                 //หาค่า TDEE
+                 if(Level_Workout=="low workout"){
+                     val resultTDEE = (BMR * 1.2 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
+
+                 }else if(Level_Workout=="normal workout to 1-3 time a week"){
+                     val resultTDEE = (BMR * 1.375 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+
+                     Log.d("information","TDEE :$TDEE")
+                 }else if (Level_Workout=="normal workout to 4-5 time a week"){
+                     val resultTDEE = (BMR * 1.55 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+
+                     Log.d("information","TDEE :$TDEE")
+                 }else if (Level_Workout=="heavy workout to 6-7 time a week"){
+                     val resultTDEE = (BMR * 1.7 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
+                 }else{
+                     val resultTDEE = (BMR * 1.9 )
+                     val reTDEE = Math.round(resultTDEE)
+                     val TDEE = reTDEE - 500
+                     Log.d("information","TDEE :$TDEE")
+                 }
+
+             }
 
 
-            //หาBMI
-            if(BMI < 23){
-                var status = "ปหติ"
-                Log.d("information","ร่างกายปกติ = $BMIS")
-                Log.d("information","$Reheight")
-                Log.d("information","$weigth")
-                Log.d("information","$height")
-            }else if( BMI >= 23 && BMI <=25){
-                var status = "เริ่มอ่วน"
-                Log.d("information","ร่างกายเริ่มอ่วน = $BMIS")
-                Log.d("information","$Reheight")
-                Log.d("information","$weigth")
-            }else if( BMI >= 25 && BMI <=30) {
-                var status = "อ่วน"
-                Log.d("information", "ร่างกายอ่วน = $BMIS")
-                Log.d("information","$Reheight")
-                Log.d("information","$weigth")
-            }else{
-                var status = "อ่วนมาก"
-                Log.d("information", "ร่างกายอ่วนมาก = $BMIS")
-                Log.d("information","$Reheight")
-                Log.d("information","$weigth")
-            }
-
-            //หาBMR
-
-
-
-
-            val intent = Intent(this, Totalkcal_User::class.java)
-            startActivity(intent)
-
+val user = User(textfirstname,textlastname,BMI,age,gender,Level_Workout)
+            Log.d("information","User :$user")
+            val uid = FirebaseAuth.getInstance().uid?:""
+            val myRef= FirebaseDatabase.getInstance().getReference("/users/$uid")
+            myRef.setValue(user)
+                .addOnSuccessListener {
+                    Log.e("register", "save to database")
+                    val intent = Intent(this, Totalkcal_User::class.java)
+                    startActivity(intent)
+                }
         }
-
-
 
 }
 
-   /* fun genderboy(weigth:Float,height:Float,age:Float){
-        val result = 66+(13.7f*weigth)+(5*height)-(6.8f*age)
-        val df = DecimalFormat("0,000")
-        df.roundingMode = RoundingMode.CEILING
-        val textBMR = df.format(result)
-        val number = java.lang.Float.valueOf(textBMR)
-        Log.d("information","BMRชาย:$textBMR Kcal")
-        Log.d("information","number:$number Kcal")
-    }*/
-
-    /*fun testTDEE(result:Float,resultSPIN:String){
-        val df = DecimalFormat("0,000")
-     val textBMR = df.format(result)
-        val BMR = java.lang.Float.valueOf(textBMR)
-        if(resultSPIN == "normal workout to 4-5 time a week"){
-            val df = DecimalFormat("0,000")
-            val resultTDEE = 500-(BMR * 1.55)
-            val TDEE = df.format(resultTDEE)
-            Log.d("information","TDEE:$TDEE Kcal")
-        }
-    }*/
-
-
 }
+    class User(val textfirstname:String ,val textlastname:String,BMI:Float,age:Float,gender:String,Level_Workout:String)
--- app/src/main/java/com/example/healthy_body/Register_User.kt
+++ app/src/main/java/com/example/healthy_body/Register_User.kt
@@ -7,41 +7,54 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_register__user.*
 
 class Register_User : AppCompatActivity() {
 
-  //  private var myRef = FirebaseAuth.getInstance()
+    private var myRef = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register__user)
 
 
-      //  val button1 = findViewById(R.id.button1) as Button
+        val button1 = findViewById(R.id.button1) as Button
         button1.setOnClickListener {
-           // val email = email.text.toString()
-        //    val password = password.text.toString()
-        //    val conpassword = conpassword.text.toString()
-       //    myRef = FirebaseAuth.getInstance()
+            val email = email.text.toString()
+            val password = password.text.toString()
+            val conpassword = conpassword.text.toString()
+           myRef = FirebaseAuth.getInstance()
 
-            //if(email!=""&&password!=""&&conpassword!=""){
-          //      if(password==conpassword){
-                  //  myRef.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
-                    //    if (it.isSuccessful)
-                   //         Log.d("Main","saved")
-                        val intent = Intent(this, Register_Infirmation::class.java)
-                        startActivity(intent)
-                //    }.addOnFailureListener {
-                //        Log.d("Main","Error")
-              //      }
-            //    }else{
+            if(email!=""&&password!=""&&conpassword!=""){
+                if(password==conpassword){
+                    myRef.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                        if (it.isSuccessful)
+                            saveusertodatabase()
+
+                    }.addOnFailureListener {
+                        Log.d("Main","Error")
+                   }
+               }else{
                     Toast.makeText(this, "Password and ConfrimPassword is not correct.", Toast.LENGTH_SHORT).show()
                 }
-        //    }else{
-        //        Toast.makeText(this, "Please in put Email and Password.", Toast.LENGTH_SHORT).show()
-         //   }
+            }else{
+                Toast.makeText(this, "Please in put Email and Password.", Toast.LENGTH_SHORT).show()
+            }
 
         }
     }
-//}
+    private fun saveusertodatabase(){
+        val uid = FirebaseAuth.getInstance().uid?:""
+        val myRef= FirebaseDatabase.getInstance().getReference("/users/$uid")
+        val user = UserLogin(uid,email.text.toString(),password.text.toString())
+        myRef.setValue(user)
+            .addOnSuccessListener {
+                Log.e("register","save to database")
+                val intent = Intent(this, Register_Infirmation::class.java)
+                startActivity(intent)
+            }
+    }
+}
+class UserLogin(val uid:String ,val email:String ,val password:String)
+
