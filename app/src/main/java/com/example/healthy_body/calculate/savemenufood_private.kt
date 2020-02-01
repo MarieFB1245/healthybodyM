a42ea966b652373ae60cf6b0d30aa8c6a06db377
MarieData <marie.fb1245@gmail.com>
2020-02-02 00:37:54 +0700

    refactor(Mobile):edit bug and add private ui

---
+++ app/src/main/java/com/example/healthy_body/calculate/savemenufood_private.kt
@@ -0,0 +1,78 @@
+package com.example.healthy_body.calculate
+
+import android.util.Log
+import com.example.healthy_body.model.modelsavefood
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+
+class savemenufood_private(val UID :String ,var namefood:String="",val kcal:String="",val unit:String="",val unittype:String="",val amount :Int){
+
+    val ref = FirebaseDatabase.getInstance().getReference("DATA_PRIVATE_FOOD")
+    val namefoodS = namefood
+    val kcalS = kcal
+    val unitS = unit
+    val unittypeS = unittype
+    val amountS = amount
+    val uid = UID
+    fun save(){
+
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {}
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists()){
+                    for(list in p0.children) {
+                        val s = list!!.getValue(data::class.java)
+                        val key = s!!.id_food
+                        val namefood = s!!.namefood
+                        val amount = s!!.amount
+                        val kcal = s!!.kcal
+                        val util = s!!.unit
+                        val unittype = s!!.unittype
+
+                        val ref = FirebaseDatabase.getInstance().getReference("DATA_PRIVATE_FOOD")
+                        val q = ref.orderByKey().limitToLast(1)
+                        q.addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onCancelled(p0: DatabaseError) {
+                            }
+
+                            override fun onDataChange(p0: DataSnapshot) {
+                                for (list in p0.children) {
+                                    val s = list.getValue(data_private::class.java)
+                                    val stringkey = s!!.id_food
+                                    val intkey = stringkey!!.toInt()
+                                    val newkey = intkey + 1
+                                    val id_food = newkey.toString()
+                                    val model = modelsavefood(
+                                        id_food,
+                                        namefoodS,
+                                        kcalS,
+                                        unitS,
+                                        unittypeS,
+                                        amountS.toString()
+                                    )
+                                    ref.child("${uid}").child("${id_food}").setValue(model)
+                                }
+
+
+                            }
+                        })
+
+                    }
+                }else {
+                    val amount = amount.toString()
+                    val key: Int = 0
+                    val newkey = key + 1
+                    val id_food = newkey.toString()
+                    val model = modelsavefood(id_food,namefood,kcal,unit,unittype,amount)
+                    ref.child("${uid}").child("${id_food}").setValue(model)
+                }
+            }
+        })
+    }
+
+
+}
+class data_private (val id_food:String ="",val namefood: String="",val amount: String="",val kcal :String="" ,val unit: String ="",val unittype: String="")
--- app/src/main/java/com/example/healthy_body/selectelistfood_user_private.kt
+++ app/src/main/java/com/example/healthy_body/selectelistfood_user_private.kt
@@ -6,29 +6,26 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import android.widget.ImageView
-import cn.pedant.SweetAlert.SweetAlertDialog
 import com.example.healthy_body.model.modellistfood
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.database.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
-import kotlinx.android.synthetic.main.activity_home__user.*
 import kotlinx.android.synthetic.main.activity_selectlistfood_user.*
-import kotlinx.android.synthetic.main.barselect.*
 import kotlinx.android.synthetic.main.list_food.view.*
 
-class selectlistfood_user : AppCompatActivity() {
-    val UID="Ph0BSgJTuLUluUI7IpGMcDPCeBx2"
-    val ref = FirebaseDatabase.getInstance().getReference("FOOD")
-   // var UID :String=""
+class selectelistfood_user_private : AppCompatActivity() {
 
+    //var UID :String="GRp37lrFluTK2OhZpUc5dTg0Ofa2"
+
+    var UID :String=""
     var searchtext :String=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selectlistfood_user)
+        setContentView(R.layout.activity_selectelistfood_user_private)
 
-       //  UID = intent.getStringExtra("UID")
+        UID = intent.getStringExtra("UID")
 
         Log.e("UID =>","${UID}")
         val adapter = GroupAdapter<ViewHolder>()
@@ -38,7 +35,7 @@ class selectlistfood_user : AppCompatActivity() {
 
         var search = findViewById<EditText>(R.id.Searching)
         sreachtext.setOnClickListener {
-             searchtext = search.text.toString()
+            searchtext = search.text.toString()
             loadfood(searchtext)
         }
 
@@ -47,16 +44,9 @@ class selectlistfood_user : AppCompatActivity() {
 
         val arrow = findViewById<ImageView>(R.id.arrow)
         val imageadd = findViewById<ImageView>(R.id.addfood)
-        val imagelist = findViewById<ImageView>(R.id.addprivate)
         val tooltset = findViewById<androidx.appcompat.widget.Toolbar>(R.id.app_bar1)
         setSupportActionBar(tooltset)
 
-
-        imagelist.setOnClickListener {
-            val intent = Intent(this,selectelistfood_user_private::class.java)
-            intent.putExtra("UID",UID)
-            startActivity(intent)
-        }
 
         arrow.setOnClickListener {
             val intent = Intent(this, Home_User::class.java)
@@ -65,16 +55,19 @@ class selectlistfood_user : AppCompatActivity() {
         }
 
         imageadd.setOnClickListener {
-            val intent = Intent(this, addfood_user::class.java)
+            val intent = Intent(this, addfood_user_private::class.java)
             intent.putExtra("UID",UID)
             startActivity(intent) }
 
-        }
+    }
+
+
 
 
 
 
     fun loadfood(s: String) {
+        val ref = FirebaseDatabase.getInstance().getReference("DATA_PRIVATE_FOOD").child("${UID}")
         if (s != null) {
             val firebaseSrarchQuery: Query =
                 ref.orderByChild("namefood").startAt(s).endAt(s + "\uf8ff")
@@ -82,35 +75,23 @@ class selectlistfood_user : AppCompatActivity() {
             val adapter = GroupAdapter<ViewHolder>()
             firebaseSrarchQuery.addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(p0: DataSnapshot) {
-                    if(p0.exists()){
-                        p0.children.forEach {
+                    p0.children.forEach {
                         Log.d("text", it.toString())
                         val food = it.getValue(modellistfood::class.java)
                         if (food != null) {
                             adapter.add(Food(food))
                         }
                     }
-                        adapter.setOnItemClickListener { item, view ->
-                            val fooditem = item as Food
-                            val intent = Intent(view.context, savedatafood_user::class.java)
-                            intent.putExtra("UID",UID)
-                            intent.putExtra("namefood", fooditem.food.namefood)
-                            intent.putExtra("kcalfood", fooditem.food.kcal)
-                            intent.putExtra("id", fooditem.food.id_food)
-                            startActivity(intent)
-                        }
-                        mRecycleVeiew.adapter = adapter
-                    }else{
-                        SweetAlertDialog(this@selectlistfood_user, SweetAlertDialog.WARNING_TYPE)
-                            .setTitleText("ไม่ค้นพบที่ค้นหา")
-                            .setContentText("ต้องการเพิ่มลงในรายการหรือไม่?")
-                            .setCancelText("ไม่ต้องการ!")
-                            .setConfirmText("ต้องการ!")
-                            .showCancelButton(true)
-                            .setCancelClickListener { sDialog -> sDialog.cancel() }
-                            .show()
+                    adapter.setOnItemClickListener { item, view ->
+                        val fooditem = item as Food
+                        val intent = Intent(view.context, savedatafood_user::class.java)
+                        intent.putExtra("UID",UID)
+                        intent.putExtra("namefood", fooditem.food.namefood)
+                        intent.putExtra("kcalfood", fooditem.food.kcal)
+                        intent.putExtra("id", fooditem.food.id_food)
+                        startActivity(intent)
                     }
-
+                    mRecycleVeiew.adapter = adapter
                 }
 
                 override fun onCancelled(p0: DatabaseError) {
@@ -151,10 +132,10 @@ class selectlistfood_user : AppCompatActivity() {
 
 
 
-    }
+        }
 
     }
-   inner class Food(val food: modellistfood) : Item<ViewHolder>() {
+    inner class Food(val food: modellistfood) : Item<ViewHolder>() {
         override fun bind(viewHolder: ViewHolder, position: Int) {
             Log.d("name_food", food.namefood)
             Log.d("kcal_String", food.kcal)
@@ -169,6 +150,5 @@ class selectlistfood_user : AppCompatActivity() {
             return R.layout.list_food
         }
     }
-
 
 }
